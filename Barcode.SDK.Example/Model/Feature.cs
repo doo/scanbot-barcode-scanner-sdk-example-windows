5956f6a1d96709ec4abc0af4f790a121b040dbc9
Nikituh <aare.undo@gmail.com>
2022-08-12 12:52:56 +0300

    featureId fix

--- Barcode.SDK.Example/Model/Feature.cs
+++ Barcode.SDK.Example/Model/Feature.cs
@@ -11,8 +11,8 @@ namespace Barcode.SDK.Example.Model
         public static List<Feature> List = new List<Feature>
         {
             new Feature(0, "barcode-scanner", "Barcode Scanner RTU-UI", "Ready-made Scanbot Camera page: Capture and retrieve a single Barcode Result"),
-            new Feature(0, "barcode-scanner", "Barcode Scanner Layout", "Custom BarcodeScannerComponent implementation with endless recognition"),
-            new Feature(1, "import", "Import Barcode Image", "Import an existing image containing one or more barcodes"),
+            new Feature(1, "barcode-scanner", "Barcode Scanner Layout", "Custom BarcodeScannerComponent implementation with endless recognition"),
+            new Feature(2, "import", "Import Barcode Image", "Import an existing image containing one or more barcodes"),
         };
 
         public int Id { get; set; }
