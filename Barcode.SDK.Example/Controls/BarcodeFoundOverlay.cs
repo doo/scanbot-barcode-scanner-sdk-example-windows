41d060d8445d986b19b80e1499ce8f8b523e44e7
Nikituh <aare.undo@gmail.com>
2022-10-06 11:57:56 +0300

    proper styling for barcode result overlay

--- Barcode.SDK.Example/Controls/BarcodeFoundOverlay.cs
+++ Barcode.SDK.Example/Controls/BarcodeFoundOverlay.cs
@@ -1,10 +1,12 @@
 ﻿using Scanbot.Model;
+using Scanbot.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace BarcodeSDK.Example.Controls
 {
@@ -20,18 +22,28 @@ namespace BarcodeSDK.Example.Controls
             TextBlock = new TextBlock();
             TextBlock.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
             TextBlock.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center;
+            TextBlock.FontSize = 20.0;
+            TextBlock.FontWeight = Windows.UI.Text.FontWeights.Bold;
+            TextBlock.Margin = new Windows.UI.Xaml.Thickness(0, 0, 0, 20);
             Children.Add(TextBlock);
 
             Continue = new Button();
             Continue.Content = "Continue";
             Continue.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
             Continue.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center;
+            Continue.FontSize = 18.0;
+            Continue.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            Continue.Margin = new Windows.UI.Xaml.Thickness(0, 0, 0, 0);
             Children.Add(Continue);
 
             Close = new Button();
             Close.Content = "Close";
             Close.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
             Close.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center;
+            Close.Background = new SolidColorBrush(Windows.UI.Colors.Transparent);
+            Close.Margin = new Windows.UI.Xaml.Thickness(0, 15, 0, 0);
+            Close.FontSize = 16.0;
+
             Children.Add(Close);
 
             HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
@@ -43,7 +55,12 @@ namespace BarcodeSDK.Example.Controls
         {
             Visibility = Windows.UI.Xaml.Visibility.Visible;
 
-            TextBlock.Text = result.ToString();
+            string text = "";
+            foreach(var item in result.Barcodes)
+            {
+                text += item.Text + " (" + item.Type.Description() + "), ";
+            }
+            TextBlock.Text = text.TrimEnd(' ').TrimEnd(',');
         }
 
         public void Hide()
